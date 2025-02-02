9a401b956d4d424e3a311ffdec3c8e390a7e8e2b
ghubo <poselektronikku@gmail.com>
2025-02-02 22:08:33 +0700

    UPDATE: animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,16 @@ module.exports = {
 			  dash: {
 				to: { strokeDashoffset: 0 }
 			  },
+			  fade: {
+				from: {
+					opacity: 0,
+					transform: 'scale(0.9)'
+				},
+				to: {
+					opacity: 1,
+					transform: 'scale(1)'
+				}
+			  }
 			}
 		},
 	},
