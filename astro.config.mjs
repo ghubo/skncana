dfdb5fae78665f39b292601f78758ea79a1f58e7
ghubo <poselektronikku@gmail.com>
2025-02-03 07:44:10 +0700

    UPDGRADE: astro tailwind & UPDATE: animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
   experimental: {
     svg: true,
   }
