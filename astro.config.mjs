f4e14ee88248e84889b5d3d3e25d33c4539828e9
ghubo <poselektronikku@gmail.com>
2025-02-17 20:57:16 +0700

    UPDATE: prettiered

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   experimental: {
     svg: true,
-  }
+  },
 });
